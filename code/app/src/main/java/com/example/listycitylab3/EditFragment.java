5661c739d269d847c6d24b68aca73bc26b1828be
Gurkeerat S Kakar <gskakar@ualberta.ca>
2025-09-19 09:54:53 -0600

    Implemented Lab 3

---
+++ code/app/src/main/java/com/example/listycitylab3/EditFragment.java
@@ -0,0 +1,61 @@
+package com.example.listycitylab3;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditFragment extends DialogFragment {
+
+    public interface EditCityDialogListener {
+        void editCity(City editCity);
+    }
+
+    private EditCityDialogListener listener;
+
+    public static EditFragment newInstance(City city) {
+        Bundle args = new Bundle();
+        args.putSerializable("city", city);
+        EditFragment fragment = new EditFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof EditCityDialogListener) {
+            listener = (EditCityDialogListener) context;
+        } else {
+            throw new RuntimeException(context + "must implement EditCityDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Edit city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Save", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+                    listener.editCity(new City(cityName, provinceName));
+                    dismiss();
+                })
+                .create();
+    }
+}
--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -6,31 +6,62 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener, EditFragment.EditCityDialogListener {
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private CityArrayAdapter cityAdapter;
+    private int selectedIndex = -1;
+
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        String[] cities = {"Edmonton", "Vancouver", "Toronto"};
+        String[] provinces = {"AB", "BC", "ON"};
 
-        dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
-        
+        dataList = new ArrayList<City>();
+
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedIndex = position;
+            City toEdit = dataList.get(position);
+            EditFragment.newInstance(toEdit)
+                    .show(getSupportFragmentManager(), "Edit City");
+        });
+
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> {
+            new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+        });
+    }
+
+    @Override
+    public void editCity(City editCity) {
+        if (selectedIndex >= 0 && selectedIndex < dataList.size()) {
+            dataList.set(selectedIndex, editCity);
+            cityAdapter.notifyDataSetChanged();
+        }
+        selectedIndex=-1;
     }
 }
